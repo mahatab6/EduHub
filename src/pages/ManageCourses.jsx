116de2037ff326c9018007d62c25e0bc838a8600
mahatab6 <mahatabcall56@gmail.com>
2025-06-13 09:27:17 +0600

    manage course section done

--- src/pages/ManageCourses.jsx
+++ src/pages/ManageCourses.jsx
@@ -1,21 +1,83 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router';
 import { IoAddOutline } from "react-icons/io5";
+import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
+import graduation from '../assets/graduation.png'
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+
+
 
 
 const ManageCourses = () => {
 
-    
+    const {user} = useContext(AuthContext);
+    const [singleCourse, setSingleCourse] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`http://localhost:3000/manage-courses/${user?.email}`)
+        .then(data =>
+            setSingleCourse(data.data)
+        )
+    },[user])
+
+    console.log(singleCourse)
+
     return (
-        <div>
-            <div className='flex items-center justify-around py-12'>
+        <div className='min-h-screen'>
+            <div className='flex items-center justify-around py-12 px-2'>
                 <div>
                     <h1 className='text-3xl font-bold text-gray-900'>Manage Courses</h1>
                     <p className='text-gray-600 mt-2'>Manage your created courses</p>
                 </div>
                 <Link to="/add-course" className='btn bg-purple-600 text-white rounded-xl'><IoAddOutline size={25}/> Add New Course</Link>
             </div>
-            
+
+            {
+                singleCourse.length === 0 ? (
+                    <div className='text-center py-16'>
+                    <img className='w-28 text-gray-400 mx-auto mb-4' src={graduation} alt="" />
+                    <h1 className='text-2xl font-semibold text-gray-900 mb-4'>No courses created yet</h1>
+                    <p className='text-gray-600 mb-8'>Start sharing your knowledge by creating your first course.</p>
+                    <Link to="/add-course" className='btn bg-purple-600 text-white rounded-xl'>
+                        <IoAddOutline size={25}/> Create Your First Course
+                    </Link>
+                    </div>
+                ) : (
+                    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                            <table className="table">
+                                <thead>
+                                    <div className='pt-4 pl-4 space-y-2'>
+                                        <h1 className='text-3xl font-bold text-gray-900'>Your Courses</h1>
+                                        <p className='text-gray-600 mt-2'>You have created {singleCourse.length} courses</p>
+                                    </div>
+                                    <tr>
+                                    <th className='text-left py-3 px-4 text-2xl font-bold text-black'>Title</th>
+                                    <th className='text-left py-3 px-4 text-2xl font-bold text-black'>Description</th>
+                                    <th className='text-left py-3 px-4 text-2xl font-bold text-black'>Created</th>
+                                    <th className='text-left py-3 px-4 text-2xl font-bold text-black'>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {singleCourse.map((course, index) => (
+                                    <tr key={index} className="hover:bg-base-300">
+                                        <td>{course.title}</td>
+                                        <td>{course.description}</td>
+                                        <td>{course.created}</td>
+                                        <td className='flex gap-1'>
+                                        <button className="btn rounded-xl btn-primary"><FaRegEdit />Edit</button>
+                                        <button className="btn rounded-xl bg-red-600 text-white"><MdDeleteForever />Delete</button>
+                                        </td>
+                                    </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                )   
+            }
         </div>
     );
 };
