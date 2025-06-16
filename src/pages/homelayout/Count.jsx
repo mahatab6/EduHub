9c21b330306a7a3ac934410999aef2b717e1d0f5
mahatab6 <mahatabcall56@gmail.com>
2025-06-16 06:49:46 +0600

    Lottie animation added

--- src/pages/homelayout/Count.jsx
+++ src/pages/homelayout/Count.jsx
@@ -4,6 +4,11 @@ import { LuUsers } from "react-icons/lu";
 import { FaBookOpen } from "react-icons/fa6";
 import { LiaCertificateSolid } from "react-icons/lia";
 import { FaStarHalfAlt } from "react-icons/fa";
+import Lottie from "lottie-react";
+import Students  from '../../assets/Animation - 1750033552111.json'
+import Courses from '../../assets/Animation - 1750034171266.json'
+import Certificates from '../../assets/Animation - 1750034566935.json'
+import Rating from '../../assets/Animation - 1750034830618.json'
 
 
 
@@ -13,22 +18,22 @@ const count = () => {
     return (
         <div className='py-16 bg-gray-50 grid lg:grid-cols-4 grid-cols-2 gap-4'>
             <div className='text-center justify-items-center space-y-2'>
-                <LuUsers className='w-16 h-16 bg-purple-600 rounded-full p-2 text-white ' />
+                <Lottie style={{width:'200px', }} animationData={Students}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={10000} /> <span className='text-3xl font-bold text-gray-900'>+</span>
                 <p className='text-gray-600 text-xl'>Active Students</p>
             </div>
             <div className='text-center justify-items-center space-y-2'>
-                <FaBookOpen  className='w-16 h-16 bg-purple-600 rounded-full p-2 text-white ' />
+                <Lottie style={{width:'200px', height:'200px' }} animationData={Courses}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={500} /> <span className='text-3xl font-bold text-gray-900'>+</span>
                 <p className='text-gray-600 text-xl'>Courses Available</p>
             </div>
             <div className='text-center justify-items-center space-y-2'>
-                <LiaCertificateSolid className='w-16 h-16 bg-purple-600 rounded-full p-2 text-white ' />
+                <Lottie style={{width:'200px', height:'200px' }} animationData={Certificates}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={25000} /> <span className='text-3xl font-bold text-gray-900'>+</span>
                 <p className='text-gray-600 text-xl'>Certificates Issued</p>
             </div>
             <div className='text-center justify-items-center space-y-2'>
-                <FaStarHalfAlt  className='w-16 h-16 bg-purple-600 rounded-full p-2 text-white ' />
+                <Lottie style={{width:'200px', height:'200px' }} animationData={Rating}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={4} /> <span className='text-3xl font-bold text-gray-900'>/5</span>
                 <p className='text-gray-600 text-xl'>Average Rating</p>
             </div>
