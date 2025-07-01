aa5f2be37ab6ce1b687100e914b1e814603fe071
mahatab6 <mahatabcall56@gmail.com>
2025-07-01 11:28:10 +0600

    count section add

--- src/pages/homelayout/Count.jsx
+++ src/pages/homelayout/Count.jsx
@@ -14,23 +14,23 @@ const count = () => {
     return (
         <div className='py-16 bg-gray-50 grid lg:grid-cols-4 grid-cols-2 gap-4'>
             <div className='text-center justify-items-center space-y-2'>
-                <Lottie style={{width:'200px', }} animationData={Students}  loop={true} ></Lottie>
+                <Lottie style={{width:'150px', }} animationData={Students}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={10000} /> <span className='text-3xl font-bold text-gray-900'>+</span>
                 <p className='text-gray-600 text-xl'>Active Students</p>
             </div>
             <div className='text-center justify-items-center space-y-2'>
-                <Lottie style={{width:'200px', height:'200px' }} animationData={Courses}  loop={true} ></Lottie>
+                <Lottie style={{width:'150px', height:'150px' }} animationData={Courses}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={500} /> <span className='text-3xl font-bold text-gray-900'>+</span>
                 <p className='text-gray-600 text-xl'>Courses Available</p>
             </div>
             <div className='text-center justify-items-center space-y-2'>
-                <Lottie style={{width:'200px', height:'200px' }} animationData={Certificates}  loop={true} ></Lottie>
+                <Lottie style={{width:'150px', height:'150px' }} animationData={Certificates}  loop={true} ></Lottie>
                 <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={25000} /> <span className='text-3xl font-bold text-gray-900'>+</span>
                 <p className='text-gray-600 text-xl'>Certificates Issued</p>
             </div>
             <div className='text-center justify-items-center space-y-2'>
-                <Lottie style={{width:'200px', height:'200px' }} animationData={Rating}  loop={true} ></Lottie>
-                <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={4} /> <span className='text-3xl font-bold text-gray-900'>/5</span>
+                <Lottie style={{width:'150px', height:'150px' }} animationData={Rating}  loop={true} ></Lottie>
+                <CountUp className='text-3xl font-bold text-gray-900 mb-2' end={99} /> <span className='text-3xl font-bold text-gray-900'>/100</span>
                 <p className='text-gray-600 text-xl'>Average Rating</p>
             </div>
         </div>
