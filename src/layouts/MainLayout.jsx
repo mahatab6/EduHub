8a3fa2c0f2e99d50502173edb3dca5137d6875fe
mahatab6 <mahatabcall56@gmail.com>
2025-07-01 12:35:49 +0600

    scrooll to top

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../components/Footer';
+import ScrollToTop from '../routes/ScrollToTop';
 
 const MainLayout = () => {
     return (
         <div>
+            <ScrollToTop/>
             <header className='sticky top-0 z-50'>
                 <Navbar/>
             </header>
